8030f74dd08559cd66c590c7511fe39c672ab8d1
ZubarevAN <ZubarevAN@polyus.com>
2026-02-17 01:32:26 +0300

    init

--- local/templates/.default/components/osinit/form.result.new/faq/.description.php
+++
@@ -1,8 +0,0 @@
-<?
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-
-$arTemplateDescription = array(
-    "NAME" => "FAQ Form Template",
-    "DESCRIPTION" => "Custom template for FAQ form integration with sector-specific layout.",
-);
-?>
